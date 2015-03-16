e9343bfaa4483472f64d1f8634b5def3e40c9747
Raphael Viera Miollo <raphaelvmiollo@miollo.(none)>
2015-03-16 11:36:42 -0300

    Lendo arquivo de notas e salvando no vetor_nota

--- app.c
+++ app.c
@@ -37,11 +37,47 @@ int main(int argc, char *argv[]){
 	//Conta as linhas do arquivo de alunos para saber quantos registros exsitem 
 	while(!feof(alunos)){
 		c = fgetc(alunos);
-        if( c == '\n')
-            n_linhas++;
+		if( c == '\n')
+			n_linhas++;
 	}
 
+	//Verifica se o número de registros não excede o permitido
+	if(n_linhas > 50){
+		fprintf(stderr, "Número de registros do arquivo de alunos doi excedido! \n");
+		return 0;
+	}
+
+	//Lê os registros de alunos para o vetor_alunos
+	/*fseek(alunos, 0, SEEK_SET); 
+	for(i=0; i<= n_linhas; i++){
+		fscanf(alunos, "%d", &vetor_aluno[i] );
+
+	}*/
 
 
+	//Abre o arquivo de notas e arvazena em no vetor_notas
+	notas = fopen("notas.txt", "r");
+	if(notas == NULL){
+		fprintf(stderr, "Arquivo de notas não está acessivel \n");
+		return 0;
+	}
+
+	while(!feof(alunos)){
+		c = fgetc(alunos);
+		if( c == '\n')
+			n_linhas++;
+	}
+
+	fseek(notas, 0, SEEK_SET);
+	for(i=0; i<=n_linhas; i++){
+		fscanf(notas, "%d ", &vetor_nota[i].matricula);
+		fscanf(notas, "%f ", &vetor_nota[i].nota1);
+		fscanf(notas, "%f", &vetor_nota[i].nota2);
+	}
+
+	printf("%d", vetor_nota[1].matricula);
 
 }
+
+//Criar função para exibir medias;
+
