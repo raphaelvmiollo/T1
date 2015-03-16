56f9c905373f38ad2ec8e33255ac75357761ac2f
root <root@miollo.(none)>
2015-03-15 23:07:48 -0300

    Estruturas criadas e validação de parametros

--- app.c
+++ app.c
@@ -1,5 +1,28 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(){
+//Estrutura que irá armazenar os dados dos alunos
+struct aluno{
+	char nome[50];
+	int matricula;
+};
+
+//Estrutura que irá armazenar a matricula e as notas dos alunos
+struct nota{
+	int matricula;
+	float nota1;
+	float nota2;
+};
+
+
+int main(int argc, char *argv[]){
+
+	//Verifica se o número de parametros passados estão corretos
+	if (argc != 2){
+		fprintf(stderr, "Informe apenas o nome do aluno desejado \n");
+	}
+
+
+
 
 }
