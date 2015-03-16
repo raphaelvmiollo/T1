6b13e3868670fcfd8834fa8d6c57a8fe7f3a7bc1
Raphael Miollo <rmiollo@inf.ufsm.br>
2015-03-16 00:30:28 -0300

    Lendo arquivo alunos e contando registros

--- app.c
+++ app.c
@@ -14,12 +14,31 @@ struct nota{
 	float nota2;
 };
 
-
 int main(int argc, char *argv[]){
+
+	FILE *notas, *alunos;
+	int c, n_linhas = 0, i;
+	struct nota vetor_nota[50];
+	struct aluno vetor_aluno[50];
 
 	//Verifica se o número de parametros passados estão corretos
 	if (argc != 2){
 		fprintf(stderr, "Informe apenas o nome do aluno desejado \n");
+		return 0;
+	}
+
+	//Abre o arquivo de alunos e verifica se o arquivo foi aberto corretamente
+	alunos = fopen("alunos.txt", "r");
+	if(alunos == NULL){
+		fprintf(stderr, "Arquivo de alunos não está acessivel! \n" );
+		return 0;
+	}
+
+	//Conta as linhas do arquivo de alunos para saber quantos registros exsitem 
+	while(!feof(alunos)){
+		c = fgetc(alunos);
+        if( c == '\n')
+            n_linhas++;
 	}
 
 
